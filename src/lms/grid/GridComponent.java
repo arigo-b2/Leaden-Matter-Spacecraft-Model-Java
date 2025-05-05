b6488d66fe18b20c9c73b80df62ab6527e6f6747
arigo-b2 <arbinagotame2056@gmail.com>
2025-05-05 18:53:00 +1000

    Update GridComponent.java
    JavaDoc Add for main function

--- src/lms/grid/GridComponent.java
+++ src/lms/grid/GridComponent.java
@@ -1,5 +1,12 @@
 package lms.grid;
 
+/**
+ * An interface for components that can be encoded as strings and placed in a grid.
+ *
+ * @provided
+ */
 public interface GridComponent {
+
+
     String getEncoding();
 }
