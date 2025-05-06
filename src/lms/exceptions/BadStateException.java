7ac4a3c8ab4f83871f675319dd03cb97e47cc352
arigo-b2 <arbinagotame2056@gmail.com>
2025-05-06 10:31:19 +1000

    Update BasStateException.java, added JavaDocs
    Add JavaDocs on the main class

--- src/lms/exceptions/BadStateException.java
+++ src/lms/exceptions/BadStateException.java
@@ -2,7 +2,10 @@ package lms.exceptions;
 
 import java.io.Serializable;
 
-
+/***
+ * Special Runtime exception - It is a Runtime (unchecked exception) which is unlike the normal `extends Exception`
+ * because it needs to be able to be added to a method without needing to have it in a method signature.
+ */
 public class BadStateException extends RuntimeException implements Serializable {
 
 
