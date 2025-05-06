6e680e2539312bc878fe258d8316706a89355d36
arigo-b2 <arbinagotame2056@gmail.com>
2025-05-06 10:28:42 +1000

    Update BadStateException.java
    Constructs a new BadStateException with the specified error message.

--- src/lms/exceptions/BadStateException.java
+++ src/lms/exceptions/BadStateException.java
@@ -5,9 +5,16 @@ import java.io.Serializable;
 
 public class BadStateException extends RuntimeException implements Serializable {
 
+
     public BadStateException() {
         super();
     }
+
+
+    public BadStateException(String message) {
+        super(message);
+    }
+
 
 
 
