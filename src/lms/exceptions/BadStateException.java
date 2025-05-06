e21a14c8ce664c361d2a32eea8036e3231cc496b
arigo-b2 <arbinagotame2056@gmail.com>
2025-05-06 10:27:13 +1000

    Update BasstateException.java
    Created BadStateException that inherits from parent

--- src/lms/exceptions/BadStateException.java
+++ src/lms/exceptions/BadStateException.java
@@ -2,7 +2,13 @@ package lms.exceptions;
 
 import java.io.Serializable;
 
+
 public class BadStateException extends RuntimeException implements Serializable {
+
+    public BadStateException() {
+        super();
+    }
+
 
 
 }
