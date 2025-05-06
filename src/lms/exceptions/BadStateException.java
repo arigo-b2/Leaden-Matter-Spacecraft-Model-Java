93d9547846800e91fa0b7edfc6f0eb120c67c2a3
arigo-b2 <arbinagotame2056@gmail.com>
2025-05-06 10:31:59 +1000

    Update BasStateException, added JavaDoc for all functions
    Update BasStateException, added JavaDoc for all functions

--- src/lms/exceptions/BadStateException.java
+++ src/lms/exceptions/BadStateException.java
@@ -8,22 +8,34 @@ import java.io.Serializable;
  */
 public class BadStateException extends RuntimeException implements Serializable {
 
-
+    /***
+     * Constructs a new BadStateException with no message.
+     */
     public BadStateException() {
         super();
     }
 
-
+    /***
+     * Constructs a new BadStateException with the specified error message.
+     * @param message A String containing the error message to be associated with this exception.
+     */
     public BadStateException(String message) {
         super(message);
     }
 
-
+    /**
+     * Constructs a new BadStateException with the specified detail message and cause.
+     * @param message the detail message for exception
+     * @param cause the cause for exception
+     */
     public BadStateException(String message, Throwable cause) {
         super(message, cause);
     }
 
-
+    /**
+     * Constructs a new BadStateException with the specified cause.
+     * @param cause the cause for exception
+     */
     public BadStateException(Throwable cause) {
         super(cause);
     }
