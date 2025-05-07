b893ba2d90f93523cbaabd7136c376b6db23a0de
arigo-b2 <arbinagotame2056@gmail.com>
2025-05-07 10:59:45 +1000

    Update UnsupportedActionException.java
    Added a public function that inherits from RuntimeException

--- src/lms/exceptions/UnsupportedActionException.java
+++ src/lms/exceptions/UnsupportedActionException.java
@@ -4,5 +4,12 @@ import java.io.Serializable;
 
 public class UnsupportedActionException extends RuntimeException implements Serializable {
 
+    /**
+     * Constructs a new UnsupportedActionException with no message.
+     */
+    public UnsupportedActionException() {
+        super();
+    }
+
 
 }
