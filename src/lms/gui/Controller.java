1efe88b4ab35ec5b98c895a4e8b457bb08ec1dd7
arigo-b2 <arbinagotame2056@gmail.com>
2025-05-10 20:39:49 +1000

    Add public class Controller
    Add public class Controller

---
+++ src/lms/gui/Controller.java
@@ -0,0 +1,27 @@
+package lms.gui;
+
+import lms.exceptions.BadStateException;
+import lms.logistics.Path;
+import lms.logistics.Transport;
+import lms.grid.GameGrid;
+import lms.grid.GridComponent;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Controller class for the GUI.
+ * <p>
+ * Used to control the solution.View based on user input.
+ */
+public class Controller {
+
+    /**
+     * View for the canvas application.
+     */
+    private final ViewModel viewModel;
+
+}
