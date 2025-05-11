fcd57c61115e319f02d30cc4c06c1cb95b792c32
arigo-b2 <arbinagotame2056@gmail.com>
2025-05-11 23:07:43 +1000

    Added imports, screenwidth and screenheight values, added parameters
    Added imports, screenwidth and screenheight values, added parameters

---
+++ src/lms/gui/MainApplication.java
@@ -0,0 +1,36 @@
+package lms.gui;
+
+import lms.exceptions.FileFormatException;
+import lms.grid.GameGrid;
+import lms.io.GameLoader;
+
+import javax.swing.*;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Main application class which starts the application and sets the root frame.
+ *
+ */
+public class MainApplication {
+    /**
+     * maintains the screen Width
+     */
+    private final int SCREEN_WIDTH;
+
+    /**
+     * maintains the screen height
+     */
+    private final int SCREEN_HEIGHT;
+
+    /**
+     * Instantiates the view and controller for this application.
+     *
+     * @param title  String setting the title for the panel
+     * @param width  int setting the width for the panel
+     * @param height int setting the height for the panel
+     * @param save   String setting the save file to load
+     */
+    
+
+}
