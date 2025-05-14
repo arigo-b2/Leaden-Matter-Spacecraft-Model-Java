6be3acd0fc90a8479c694e241e3ea654a561bcbf
arigo-b2 <arbinagotame2056@gmail.com>
2025-05-14 23:43:19 +1000

    Added required imports and A JPanel that provides a graphics context for drawing the grid
    Added required imports and A JPanel that provides a graphics context for drawing the grid

---
+++ src/lms/gui/GraphicsCanvas.java
@@ -0,0 +1,26 @@
+package lms.gui;
+
+import lms.logistics.Item;
+import lms.logistics.Path;
+import lms.logistics.Transport;
+import lms.logistics.container.Receiver;
+import lms.logistics.container.Producer;
+import lms.grid.Coordinate;
+import lms.grid.GameGrid;
+import lms.grid.GridComponent;
+import lms.grid.Orientation;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.*;
+import java.util.List;
+
+/**
+ * A JPanel that provides a graphics context for drawing the grid
+ * This class is intended for use as a canvas for 2Dgraphics.
+ */
+class GraphicsCanvas extends JPanel {
+    
+}
