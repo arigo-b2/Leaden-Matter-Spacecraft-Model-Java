cfd32687f890e12f483ac2f37690c3fa18fe87bc
arigo-b2 <arbinagotame2056@gmail.com>
2025-06-07 23:38:41 +1000

    Create ViewModel.java
    Create ViewModel.java and added imports

---
+++ src/lms/gui/ViewModel.java
@@ -0,0 +1,10 @@
+package lms.gui;
+
+import lms.grid.Coordinate;
+import lms.grid.GameGrid;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
